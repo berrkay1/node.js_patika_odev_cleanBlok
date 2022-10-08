3e982575cd43bdd527bf1635c83fcb28917557c9
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-09 02:39:36 +0300

    routing done

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
-
+const path = require("path");
+const ejs = require("ejs");
 const app = express();
 
+// templete engine
+app.set("view engine","ejs");
+// views ile temp ismi değişir
 
-const photo = { id: 1, title: "Blog title", description: "Blog description" };
+
+//middleware
+app.use(express.static('public'));
 
 app.get("/",(req,res)=> {
-   res.send(photo);
+    res.render("index");
+});
+
+app.get("/post",(req,res)=> {
+    res.render("post");
+})
+
+app.get("/about",(req,res)=> {
+    res.render("about");
+})
+
+app.get("/add_post",(req,res)=> {
+    res.render("add_post");
 })
 
 
