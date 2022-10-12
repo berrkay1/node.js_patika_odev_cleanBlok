ec65ec72ea879d5beb44ed63d8daae07a0b23653
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-13 01:33:48 +0300

    to home add title and detail

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const path = require("path");
 const ejs = require("ejs");
+const Post = require("./modals/Post");
+
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 // templete engine
 app.set("view engine","ejs");
@@ -10,9 +16,14 @@ app.set("view engine","ejs");
 
 //middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/",(req,res)=> {
-    res.render("index");
+app.get("/",async (req,res)=> {
+    const posts = await Post.find({});
+    res.render("index",{
+        posts
+    });
 });
 
 app.get("/post",(req,res)=> {
@@ -27,6 +38,11 @@ app.get("/add_post",(req,res)=> {
     res.render("add_post");
 })
 
+
+app.post("/postAdd",async (req,res)=> {
+    await Post.create(req.body)
+    res.redirect("/");
+})
 
 app.listen(3000,()=> {
     console.log("sunucu başladı");
